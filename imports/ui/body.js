7d1cd3a7f0222c94c8ec1741d8246cf278ccb5de
mangaLloyd <kpverastigue@up.edu.ph>
2017-05-19 21:49:33 +0800

    Fixed bullets in tasks

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,7 +2,7 @@ import { Template } from 'meteor/templating';
 
 import './body.html';
 
-Templete.body.helpers({
+Template.body.helpers({
   tasks: [
     { text: 'This is task 1' },
     { text: 'This is task 2' },
