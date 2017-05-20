668638b25ce82c061d264c924cfb1ccff8974d6a
mangaLloyd <kpverastigue@up.edu.ph>
2017-05-20 22:09:51 +0800

    Added form for add task

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,5 +5,27 @@ import './body.html';
 Template.body.helpers({
   tasks() {
     return Tasks.find({});
+;
   },
 });
+
+Template.body.events({
+  'submit .new-task'(event) {
+    console.log(event);
+    //prevent default browser form submit
+    event.preventDefault();
+
+    //get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    //insert task into collection
+    Tasks.insert({
+      text,
+      createdAt: new Date() //current time
+    })
+
+    //clear form
+    target.text.value = "";
+  }
+})
