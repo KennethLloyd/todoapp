0097d5c54f85554405a8252d51e0b4b04bc4490f
mangaLloyd <kpverastigue@up.edu.ph>
2017-05-21 12:16:21 +0800

    Added counter for incomplete tasks

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -20,6 +20,9 @@ Template.body.helpers({
     //else return all tasks
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true }}).count();
+  },
 });
 
 Template.body.events({
